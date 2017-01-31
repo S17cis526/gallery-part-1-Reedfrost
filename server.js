5a6d962de28377cc7d373cd0b88cc4dde244c12b
Reedfrost <jwildma2@ksu.edu>
2017-01-31 13:41:30 -0600

    Update server.js

--- server.js
+++ server.js
@@ -4,3 +4,15 @@
  * simple photo gallery web app.
  */
  
+"use strict;"
+
+var http = require('http');
+var port = 2000;
+
+var server = http.createServer(function (req, res) {
+  res.end("OK");
+});
+
+server.listen(port, function(){
+  console.log("Server is listening on port ", port);
+});
