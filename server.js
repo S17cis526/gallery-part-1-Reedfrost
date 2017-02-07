199d8c657c4095d0002ef1fe2a9a529eab345854
Reedfrost <jwildma2@ksu.edu>
2017-02-07 12:32:04 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,38 @@
-/**
- * server.js
- * This file defines the server for a
- * simple photo gallery web app.
- */
- 
 "use strict;"
 
 var http = require('http');
+var fs = require('fs');
 var port = 2000;
 
+function serveImage(fileName, req, res) {
+	var data = fs.readFile('images/' + fileName, function(err, data){
+		if(err) {
+			console.error(err);
+		}
+		res.setHeader('Content-Type','image/jpeg');
+		res.end(data);
+	});
+}
+
 var server = http.createServer(function (req, res) {
-  res.end("OK");
+  switch(req.url) {
+	case '/chess.jpg':
+	case '/chess.jpeg':
+	case '/chess':
+		serveImage('chess.jpg', req, res);
+		break;
+	case '/fern':
+	case '/fern.jpg':
+	case '/fern.jpeg':
+		serveImage('fern.jpg', req, res);
+		break;
+	default:
+		res.statusCode = 404;
+		res.statusMessage = "Resource not found";
+		res.end("Couldn't find it");
+  }
 });
+
 
 server.listen(port, function(){
   console.log("Server is listening on port ", port);
